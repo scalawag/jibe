489e0be26316e9b23eb8818f78c4ef4d4c5bd38d
Justin Patterson <justin@scalawag.org>
2016-09-12 08:38:18 -0500

    Make create/update user idempotent

--- src/main/scala/org/scalawag/jibe/backend/ubuntu/Commander.scala
+++ src/main/scala/org/scalawag/jibe/backend/ubuntu/Commander.scala
@@ -14,7 +14,7 @@ object Commander {
 
     def getCommand(mandate: Mandate) = mandate match {
       case CreateOrUpdateUser(user) =>
-        Command(mandate, s"useradd ${user.name}")
+        Command(mandate, createOrUpdateUser(user))
       case DeleteUser(name) =>
         Command(mandate, s"userdel ${name}")
       case CreateOrUpdateGroup(group) =>
@@ -30,4 +30,51 @@ object Commander {
     val crs = mandates.map(getCommand).map(_.perform(ssh))
     MandateResults(crs.lastOption.map(_.exitCode).getOrElse(0),crs)
   }
+
+  private[this] def createOrUpdateUser(user: User) = {
+
+    val commonOptions = mapify(
+      user.uid.map( uid => "--uid" -> uid),
+      user.comment.map( comment => "--comment" -> comment),
+      user.primaryGroup.map( gid => "--gid" -> gid),
+      user.shell.map( shell => "--shell" -> shell)
+    )
+
+    // usermod dumbly requires slightly different options than useradd
+
+    val useraddOptions = commonOptions ++ mapify(
+      user.home.map( home => "--home-dir" -> home),
+      if ( user.system ) Some("--system" -> "") else None
+    )
+
+    val usermodOptions = commonOptions ++ mapify(
+      user.home.map( home => "--home" -> home)
+    )
+
+    val useradd =
+      s"""
+         |PATH=/usr/sbin
+         |useradd ${formatOptions(useraddOptions)} ${user.name}
+      """.stripMargin.trim
+
+    val usermod =
+      if ( usermodOptions.isEmpty )
+        s"""
+           |if [ $$? == 9 ]; then
+           |  exit 0
+           |fi
+        """.stripMargin.trim
+      else
+        s"""
+           |if [ $$? == 9 ]; then
+           |  usermod ${formatOptions(usermodOptions)} ${user.name}
+           |fi
+        """.stripMargin.trim
+
+    Iterable(useradd,usermod).mkString("\n")
+  }
+
+  private[this] def mapify(raw: Iterable[(String, Any)]*): Map[String, String] = raw.flatten.toMap.mapValues(_.toString)
+
+  private[this] def formatOptions(opts: Map[String, String]) = opts.map { case (k, v) => s"$k $v" }.mkString(" ")
 }
