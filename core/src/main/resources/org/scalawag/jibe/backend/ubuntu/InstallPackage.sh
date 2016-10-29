fe9d4957994b8200222bbcb22d320ffb6a638688
Justin Patterson <justin@scalawag.org>
2016-10-28 20:06:39 -0500

    Run apt-get with -q to prevent it from trying to access the terminal

--- core/src/main/resources/org/scalawag/jibe/backend/ubuntu/InstallPackage.sh
+++ core/src/main/resources/org/scalawag/jibe/backend/ubuntu/InstallPackage.sh
@@ -1,3 +1,3 @@
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-apt-get install --assume-yes $pkg_name${pkg_version:+=$pkg_version}
+apt-get -q install --assume-yes $pkg_name${pkg_version:+=$pkg_version}
