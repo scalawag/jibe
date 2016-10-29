7e00dea238faa87b3bc76fb57ec1c364a4717f24
Justin Patterson <justin@scalawag.org>
2016-10-29 16:41:24 -0500

    Add some logging to Sessions and synchronize session creation.

--- core/src/main/scala/org/scalawag/jibe/backend/Sessions.scala
+++ core/src/main/scala/org/scalawag/jibe/backend/Sessions.scala
@@ -1,21 +1,25 @@
 package org.scalawag.jibe.backend
 
 import com.jcraft.jsch.{ChannelExec, JSch, Session, UserInfo}
+import org.scalawag.jibe.Logging
 
 object Sessions {
   private[this] val jsch = new JSch
   private[this] var sessions = Map.empty[SshInfo, Session]
 
-  def get(ssh: SshInfo): Session = {
+  def get(ssh: SshInfo): Session = synchronized {
     sessions.get(ssh) map { s =>
-      if ( s.isConnected )
+      if ( s.isConnected ) {
+        Logging.log.debug(s"reusing existing session for $ssh: $s")
         s
-      else {
+      } else {
+        Logging.log.debug(s"removing disconnected session for $ssh: $s")
         sessions -= ssh
         get(ssh)
       }
     } getOrElse {
       val s = jsch.getSession(ssh.username, ssh.hostname, ssh.port)
+      Logging.log.debug(s"created a new session for $ssh: $s")
       s.setUserInfo(new UserInfo {
         override def getPassphrase() = ""
         override def getPassword() = ssh.password
@@ -38,10 +42,13 @@ object Sessions {
   }
 
   def withChannel[A](ssh: SshInfo)(fn: ChannelExec => A): A = {
-    val c = Sessions.get(ssh).openChannel("exec").asInstanceOf[ChannelExec]
+    val s = Sessions.get(ssh)
+    val c = s.openChannel("exec").asInstanceOf[ChannelExec]
     try {
+      Logging.log.debug(s"opened new channel ${c.getId} on session for $ssh: $s")
       fn(c)
     } finally {
+      Logging.log.debug(s"disconnecting channel ${c.getId} on session for $ssh: $s")
       c.disconnect()
     }
   }
