e570c1f576fed582843131aaf190a187e7651458
Justin Patterson <justin@scalawag.org>
2016-11-03 21:31:59 -0500

    “complete” java/sbt installation example

--- core/src/main/scala/org/scalawag/jibe/Main.scala
+++ core/src/main/scala/org/scalawag/jibe/Main.scala
@@ -2,19 +2,71 @@ package org.scalawag.jibe
 
 import java.io.{File, PrintWriter}
 
-import FileUtils._
 import org.scalawag.jibe.backend.ubuntu.UbuntuCommander
 import org.scalawag.jibe.backend._
 import org.scalawag.jibe.executive.{CommanderMultiTree, ExecutionPlan, Executive}
 import org.scalawag.jibe.mandate._
 import org.scalawag.jibe.mandate.command.{Group, User}
-import org.scalawag.jibe.multitree.{MandateSequence, MandateSet}
+import org.scalawag.jibe.multitree._
+import FileUtils._
 import org.scalawag.jibe.report.cli.TextReport
 import org.scalawag.jibe.report.cli.TextReport.TextReportOptions
+
 import scala.concurrent.{Await, ExecutionContext}
-import scala.concurrent.duration.Duration
+import scala.concurrent.duration._
 
 object Main {
+
+  // A few custom mandates for installing java software
+
+  object UpdateAptGet {
+    case class UpdateAptGet() extends StatelessMandate with MandateHelpers with OnlyIdentityEquals {
+      override def takeAction(implicit context: MandateExecutionContext) = runCommand(command.UpdateAptGet(0.seconds))
+    }
+
+    def apply() = MultiTreeLeaf(new UpdateAptGet(), Some("update apt metadata"))
+  }
+
+  // Ad hoc scripting in a mandate
+  val AcceptJava8License = MultiTreeLeaf (
+    new StatelessMandate with MandateHelpers {
+      override def isActionCompleted(implicit context: MandateExecutionContext) =
+      context.commander.executeBooleanScript(
+      """PATH=/bin:/usr/bin
+        |debconf-show oracle-java8-installer | grep '* shared/accepted-oracle-license-v1-1: true'
+      """.stripMargin.trim
+      )
+
+      override def takeAction(implicit context: MandateExecutionContext) =
+      context.commander.execute(
+      """PATH=/bin:/usr/bin
+        |echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+      """.stripMargin.trim
+      )
+    },
+    Some("Accept Java 8 license")
+  )
+
+  val usesAptDatabase = CriticalSection(Semaphore(1, Some("apt")))
+
+  def InstallJava8(updateAptGet: MultiTreeLeaf = UpdateAptGet()) =
+    MandateSequence("Install Java 8",
+      WriteRemoteFile("/etc/apt/sources.list.d/webupd8team-java-trusty.list",
+                      "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main"),
+      InstallAptKey("keyserver.ubuntu.com", "7B2C3B0889BF5709A105D03AC2518248EEA14886"),
+      updateAptGet.add(usesAptDatabase),
+      AcceptJava8License,
+      InstallPackage("oracle-java8-installer").add(usesAptDatabase)
+    )
+
+  def InstallSbt(updateAptGet: MultiTreeLeaf = UpdateAptGet()) =
+    MandateSequence("Install sbt",
+      WriteRemoteFile("/etc/apt/sources.list.d/sbt.list",
+                      "deb https://dl.bintray.com/sbt/debian /"),
+      InstallAptKey("keyserver.ubuntu.com", "2EE0EA64E40A89B84B2DF73499E82A75642AC823"),
+      updateAptGet.add(usesAptDatabase),
+      InstallPackage("sbt").add(usesAptDatabase)
+    )
 
   def main(args: Array[String]): Unit = {
     Logging // trigger initialization to get the logging configured
@@ -53,28 +105,42 @@ object Main {
 
     val mandates2 = NoisyMandate
 
+    val ex = ExitWithArgument(1)
     val mandates4 = MandateSet("A",
-      ExitWithArgument(1),
+      ex,
       ExitWithArgument(2),
       MandateSet("B",
-        ExitWithArgument(3),
+        ExitWithArgument(1),
         ExitWithArgument(4)
       ),
-      ExitWithArgument(5)
+      ExitWithArgument(5),
+      ex
     )
+
+    val installPackagesWithSharedAptGetUpdate = {
+      val updateAptGet = UpdateAptGet()
+      MandateSet("install packages",
+        InstallJava8(updateAptGet),
+        InstallSbt(updateAptGet)
+      )
+    }
+
+    val installPackagesWithoutSharedAptGetUpdate = {
+      MandateSet("install packages",
+        InstallJava8(),
+        InstallSbt()
+      )
+    }
 
     try {
       val commanderMultiTrees = Seq(
-        CommanderMultiTree(commanders(0), mandates1),
-        CommanderMultiTree(commanders(1), mandates2)
-//        ,
-//        CommanderMultiTree(commanders(2), mandates1)
-//        CommanderMultiTree(commanders(1), mandates4)
+        CommanderMultiTree(commanders(0), installPackagesWithSharedAptGetUpdate),
+        CommanderMultiTree(commanders(1), installPackagesWithoutSharedAptGetUpdate)
       )
 
       val plan = new ExecutionPlan(commanderMultiTrees)
 
-//      writeFileWithPrintWriter("graph.dot")(plan.toDot)
+      FileUtils.writeFileWithPrintWriter("graph.dot")(plan.toDot)
 
       val runDir = Await.result(Executive.execute(plan, new File("results"), true)(ExecutionContext.global), Duration.Inf)
 
